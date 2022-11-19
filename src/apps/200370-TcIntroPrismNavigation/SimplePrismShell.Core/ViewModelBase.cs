fbff3cf0a12f1d5eebef34f336739d91a9ab362e
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-11-19 10:32:22 +0530

    Removed interface

--- src/apps/200370-TcIntroPrismNavigation/SimplePrismShell.Core/ViewModelBase.cs
+++ src/apps/200370-TcIntroPrismNavigation/SimplePrismShell.Core/ViewModelBase.cs
@@ -4,28 +4,13 @@ using Prism.Regions;
 
 namespace SimplePrismShell.Core
 {
-    public class ViewModelBase : BindableBase, INavigationAware
+    public class ViewModelBase : BindableBase
     {
         string _title = default!;
         public string Title
         {
             get { return _title; }
             set { SetProperty(ref _title, value); }
-        }
-
-        public virtual bool IsNavigationTarget(NavigationContext navigationContext)
-        {
-            return true;
-        }
-
-        public void OnNavigatedFrom(NavigationContext navigationContext)
-        {
-
-        }
-
-        public void OnNavigatedTo(NavigationContext navigationContext)
-        {
-
         }
     }
 }
