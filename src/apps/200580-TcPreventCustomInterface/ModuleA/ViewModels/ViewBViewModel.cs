de7263cb1ac53364bfa5e342bbf576ca90eef357
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-11-20 08:32:53 +0530

    New Project

---
+++ src/apps/200580-TcPreventCustomInterface/ModuleA/ViewModels/ViewBViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Prism.Regions;
+using SimplePrismShell.Core;
+
+namespace ModuleA.ViewModels
+{
+    public class ViewBViewModel : ViewModelBase, IAllowCloseTheTab// IConfirmNavigationRequest
+    {
+        private string _welcomeMessage = "Hello from ViewBViewModel";
+        public string WelcomeMessage
+        {
+            get { return _welcomeMessage; }
+            set { SetProperty(ref _welcomeMessage, value); }
+        }
+        public ViewBViewModel()
+        {
+            Title = "View B";
+        }
+        public override bool IsNavigationTarget(NavigationContext navigationContext)
+        {
+            return false;
+        }
+
+        //public void ConfirmNavigationRequest(NavigationContext navigationContext, System.Action<bool> continuationCallback)
+        //{
+        //    continuationCallback(false);
+        //}
+
+        public void ConfirmNavigationRequest(System.Action<bool> continuationCallback)
+        {
+            continuationCallback(true);
+        }
+    }
+}
---
+++ src/tasks/200580-TcPreventCustomInterface/ReadMe.md
@@ -0,0 +1,3 @@
+# Basic example of Tab Control. 
+- The earlier example is not currently working. The objective is to privent a given tab from closing. 
+- So in this example we will impliment a custom interface instead of the prisms IConfirmNavigationRequest.
